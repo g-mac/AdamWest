0da41a24126f69214c8a8112a724e6a3b8dcaad9
Simon Mayrshofer <mayrshofer@silpion.de>
2014-09-17 19:13:42 +0200

    fixed (merged) manifest

--- app/src/main/java/de/adamwest/routelist/RouteListActivity.java
+++ app/src/main/java/de/adamwest/routelist/RouteListActivity.java
@@ -1,6 +1,7 @@
 package de.adamwest.routelist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -8,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import de.adamwest.MapActivity;
 import de.adamwest.R;
 import de.adamwest.database.Route;
 import de.adamwest.database.TestDataCreator;
@@ -30,7 +32,7 @@ public class RouteListActivity extends Activity {
         //TestDataCreator.createNewRoutes(this);
 
 
-        ArrayList<Route> testArr = new ArrayList<Route>();
+        final ArrayList<Route> testArr = new ArrayList<Route>();
         testArr.add(new Route());
         testArr.add(new Route());
         testArr.add(new Route());
@@ -41,6 +43,12 @@ public class RouteListActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("abc", "clicked: " + position);
+                if(position==testArr.size()){
+                    Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+//                    String message = "empty message";
+//                    intent.putExtra("message", message);
+                    startActivity(intent);
+                }
             }
         });
     }
