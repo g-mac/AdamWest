b310d3c2dae84751481cfe4cf5860bb23c79ebd9
Simon Mayrshofer <mayrshofer@silpion.de>
2014-09-17 15:54:43 +0200

    added mapactivity and show current location on map

---
+++ app/src/main/java/de/adamwest/MapActivity.java
@@ -0,0 +1,124 @@
+package de.adamwest;
+
+import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapActivity extends Activity {
+
+
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        if (map != null) {
+            Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+                    .title("Hamburg"));
+            Marker kiel = map.addMarker(new MarkerOptions()
+                    .position(KIEL)
+                    .title("Kiel")
+                    .snippet("Kiel is cool")
+                    .icon(BitmapDescriptorFactory
+                            .fromResource(R.drawable.ic_launcher)));
+        }
+
+        // map is a GoogleMap object
+        map.setMyLocationEnabled(true);
+
+//        // Move the camera instantly to hamburg with a zoom of 15.
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+//        // Zoom in, animating the camera.
+//        map.animateCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 50));
+
+        setUpMap();
+
+    }
+
+    private void setUpMap() {
+//        // Enable MyLocation Layer of Google Map
+//        map.setMyLocationEnabled(true);
+
+        // Get LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Get Current Location
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+//        //set map type: satelite
+//        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // Get latitude of the current location
+        double latitude = myLocation.getLatitude();
+
+        // Get longitude of the current location
+        double longitude = myLocation.getLongitude();
+
+        // Create a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        // add marker with description
+        map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+
+        // Show the current location in Google Map
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+        // Zoom in the Google Map
+        map.animateCamera(CameraUpdateFactory.zoomTo(17), 2500, null);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
