85b637559c524914a2a67d589c1b3e09dd45321f
rose-silpion <rose@silpion.de>
2014-10-25 20:20:36 +0200

    implemented first version of route list in detailview

--- app/src/main/java/de/adamwest/holiday/holiday_detail/RouteListFragment.java
+++ app/src/main/java/de/adamwest/holiday/holiday_detail/RouteListFragment.java
@@ -5,16 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import de.adamwest.R;
+import de.adamwest.database.DatabaseManager;
+import de.adamwest.database.Route;
+
+import java.util.List;
 
 /**
  * Created by Philip on 18.10.2014.
  */
 public class RouteListFragment extends Fragment {
+
+    private ListView listView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail_route_list, container, false);
+        listView = (ListView)view.findViewById(R.id.list_view_route_list);
+        List<Route> routes = DatabaseManager.getHolidayFromId(getActivity(),((HolidayDetailActivity)getActivity()).getHolidayId()).getRouteList();
+        listView.setAdapter(new RouteListAdapter(routes, getActivity()));
 
-        return inflater.inflate(R.layout.fragment_detail_route_list, container, false);
+        return view;
     }
 }
