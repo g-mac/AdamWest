bd241bb5fde0434c91c44beadc08542581f9a9e1
rose-silpion <rose@silpion.de>
2014-11-09 19:26:26 +0100

    added imageview for image and text

--- app/src/main/java/de/adamwest/holiday/holiday_detail/EventGridAdapter.java
+++ app/src/main/java/de/adamwest/holiday/holiday_detail/EventGridAdapter.java
@@ -1,13 +1,24 @@
 package de.adamwest.holiday.holiday_detail;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import de.adamwest.R;
 import de.adamwest.database.Event;
+import de.adamwest.database.MultimediaElement;
+import de.adamwest.helper.Constants;
+import de.adamwest.helper.HelpingMethods;
+import org.w3c.dom.Text;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,10 +55,49 @@ public class EventGridAdapter extends BaseAdapter {
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view = inflater.inflate(R.layout.item_event_grid, parent, false);
+            MultimediaElement multimediaElement = ((Event)getItem(position)).getMultimediaElementList().get(0);
+            if(multimediaElement.getType().equals(Constants.TYPE_IMAGE)) {
+                Bitmap image = resizeBitMap(view, multimediaElement);
+                if(image != null) {
+                    ((ImageView)view.findViewById(R.id.image_view_grid_item)).setImageBitmap(image);
+                }
+            }
+            else if(multimediaElement.getType().equals(Constants.TYPE_TEXT)) {
+                ((ImageView)view.findViewById(R.id.image_view_grid_item)).setImageDrawable(context.getResources().getDrawable(R.drawable.text_icon));
+            }
+            ((TextView)view.findViewById(R.id.text_view_grid_event_description)).setText(multimediaElement.getDescription());
             return view;
         }
         else {
             return convertView;
         }
     }
+
+    private Bitmap resizeBitMap(View view, MultimediaElement multimediaElement) {
+
+        File imgFile = new File(multimediaElement.getPath());
+        Bitmap bitmapImage = null;
+        if(imgFile.exists()){
+            bitmapImage = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            //Drawable d = new BitmapDrawable(getResources(), myBitmap);
+
+        }
+
+        if (bitmapImage != null) {
+            DisplayMetrics displaymetrics = new DisplayMetrics();
+            ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+            int newWidth = (displaymetrics.widthPixels -8) / 2;
+            HelpingMethods.log("Breite: " + newWidth);
+            double scaleFactor = bitmapImage.getWidth() / newWidth;
+            int newHeight = new Double(bitmapImage.getHeight() / scaleFactor).intValue();
+
+            Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmapImage, newWidth, newHeight, true);
+            return resizedBitmap;
+        }
+        else {
+            return null;
+        }
+    }
+
+
 }
